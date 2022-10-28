ea46953bd9d0c1d90f90133ea045963a715c24d3
GitAutopoc <98872904+GitAutopoc@users.noreply.github.com>
2022-10-28 17:44:23 +0530

    Update CustomerController.java

--- src/main/java/com/iiht/training/eloan/controller/CustomerController.java
+++ src/main/java/com/iiht/training/eloan/controller/CustomerController.java
@@ -30,47 +30,4 @@ public class CustomerController {
 	@Autowired
 	private CustomerService customerService;
 	
-	@PostMapping("/register")
-	public ResponseEntity<UserDto> register(@Valid @RequestBody UserDto userDto){
-		return new ResponseEntity<UserDto>(customerService.register(userDto), HttpStatus.CREATED);
-	}
-	
-	@PostMapping("/apply-loan/{customerId}")
-	public ResponseEntity<LoanOutputDto> applyLoan(@PathVariable Long customerId,
-												@Valid @RequestBody LoanDto loanDto){
-		LoanOutputDto loanODto = customerService.applyLoan(customerId, loanDto);
-		if(loanODto == null) {
-			throw new CustomerNotFoundException("Customer with Id "+customerId+" not found");
-		}else
-		  return new ResponseEntity<LoanOutputDto>(loanODto,HttpStatus.OK);
-	}
-	
-	@GetMapping("/loan-status/{loanAppId}")
-	public ResponseEntity<LoanOutputDto> getStatus(@PathVariable Long loanAppId){
-		LoanOutputDto loanODto = customerService.getStatus(loanAppId);
-		if(loanODto == null) {
-			throw new LoanNotFoundException("Customer Loan with Id "+loanAppId+" not found");
-		}else
-		return new ResponseEntity<LoanOutputDto>(loanODto, HttpStatus.OK);
-	}
-	
-	@GetMapping("/loan-status-all/{customerId}")
-	public ResponseEntity<List<LoanOutputDto>> getStatusAll(@PathVariable Long customerId){
-		List<LoanOutputDto> loanODto = customerService.getStatusAll(customerId);
-		if(loanODto == null || loanODto.size() == 0) {
-			throw new LoanNotFoundException("Customer Loan with customer Id "+customerId+" not found");
-		}else
-		return new ResponseEntity<List<LoanOutputDto>>(loanODto, HttpStatus.OK);
-	}
-	
-	@ExceptionHandler(CustomerNotFoundException.class)
-	public ResponseEntity<ExceptionResponse> handler(CustomerNotFoundException ex){
-		ExceptionResponse exception = 
-				new ExceptionResponse(ex.getMessage(),
-									  System.currentTimeMillis(),
-									  HttpStatus.NOT_FOUND.value());
-		ResponseEntity<ExceptionResponse> response =
-				new ResponseEntity<ExceptionResponse>(exception, HttpStatus.NOT_FOUND);
-		return response;
-	}
 }
