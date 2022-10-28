75287ed3978b16f49971ce37e849e4034988d064
GitAutopoc <98872904+GitAutopoc@users.noreply.github.com>
2022-10-28 17:45:29 +0530

    Update ManagerController.java

--- src/main/java/com/iiht/training/eloan/controller/ManagerController.java
+++ src/main/java/com/iiht/training/eloan/controller/ManagerController.java
@@ -31,44 +31,4 @@ public class ManagerController {
 	@Autowired
 	private ManagerService managerService;
 	
-	@GetMapping("/all-processed")
-	public ResponseEntity<List<LoanOutputDto>> allProcessedLoans() {
-		return new ResponseEntity<List<LoanOutputDto>>(managerService.allProcessedLoans(), HttpStatus.OK);
-	}
-	
-	@PostMapping("/reject-loan/{managerId}/{loanAppId}")
-	public ResponseEntity<RejectDto> rejectLoan(@PathVariable Long managerId,
-												@PathVariable Long loanAppId,
-												@RequestBody RejectDto rejectDto){
-		return new ResponseEntity<RejectDto>(managerService.rejectLoan(managerId, loanAppId, rejectDto), HttpStatus.OK);
-	}
-	
-	@PostMapping("/sanction-loan/{managerId}/{loanAppId}")
-	public ResponseEntity<SanctionOutputDto> sanctionLoan(@PathVariable Long managerId,
-												@PathVariable Long loanAppId,
-												@Valid @RequestBody SanctionDto sanctionDto){
-		return new ResponseEntity<SanctionOutputDto>(managerService.sanctionLoan(managerId, loanAppId, sanctionDto), HttpStatus.OK);
-	}
-	
-	@ExceptionHandler(ManagerNotFoundException.class)
-	public ResponseEntity<ExceptionResponse> handler(ManagerNotFoundException ex){
-		ExceptionResponse exception = 
-				new ExceptionResponse(ex.getMessage(),
-									  System.currentTimeMillis(),
-									  HttpStatus.NOT_FOUND.value());
-		ResponseEntity<ExceptionResponse> response =
-				new ResponseEntity<ExceptionResponse>(exception, HttpStatus.NOT_FOUND);
-		return response;
-	}
-	
-	@ExceptionHandler(AlreadyFinalizedException.class)
-	public ResponseEntity<ExceptionResponse> handler(AlreadyFinalizedException ex){
-		ExceptionResponse exception = 
-				new ExceptionResponse(ex.getMessage(),
-									  System.currentTimeMillis(),
-									  HttpStatus.BAD_REQUEST.value());
-		ResponseEntity<ExceptionResponse> response =
-				new ResponseEntity<ExceptionResponse>(exception, HttpStatus.BAD_REQUEST);
-		return response;
-	}
 }
